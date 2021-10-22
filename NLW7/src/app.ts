e7a46d0e10b2b3f502a3a8a965d18a1919c1bf54
Jefferson Xavier <jeffersonx.xavier@gmail.com>
2021-10-22 13:55:08 -0300

    NLW7 - Add socket.io

--- NLW7/src/app.ts
+++ NLW7/src/app.ts
@@ -1,11 +1,27 @@
 import express from 'express';
+import http from 'http';
+import cors from 'cors';
+import { Server } from 'socket.io';
 import { router } from './routes';
 import 'dotenv/config';
 
 const app = express();
 
 // Middlewares
+app.use(cors());
 app.use(express.json());
 app.use(router);
 
-app.listen(4000, () => console.log('Server is running on :4000'));
+const server = http.createServer(app);
+
+server.listen(4000, () => console.log('Server is running on :4000'));
+
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+  }
+});
+
+io.on('connection', socket => {
+  console.log(`User connected in socket: ${socket.id}`);
+});
