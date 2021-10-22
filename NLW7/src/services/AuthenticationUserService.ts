442c72c28f4f4c2a14532e8f8d429bdbe89e9301
Jefferson Xavier <jeffersonx.xavier@gmail.com>
2021-10-21 22:29:02 -0300

    NLW7 - GET GitHub User

--- NLW7/src/services/AuthenticationUserService.ts
+++ NLW7/src/services/AuthenticationUserService.ts
@@ -1,11 +1,21 @@
 import axios from 'axios';
 
+interface IAccessTokenResponse {
+  access_token: string,
+}
+
+interface IUserResponse {
+  id: number,
+  avatar_url: string,
+  login: string,
+  name: string,
+}
+
 class AuthenticationUserService {
 
   async execute(code: string) {
-    const url = 'https://github.com/login/oauth/access_token';
-
-    const response = await axios.post(url, null, {
+    const { data: accessTokenResponse } = await axios.post<IAccessTokenResponse>(
+        'https://github.com/login/oauth/access_token', null, {
       params: {
         client_id: process.env.GITHUB_CLIENT_ID,
         client_secret: process.env.GITHUB_CLIENT_SECRET,
@@ -13,6 +23,12 @@ class AuthenticationUserService {
       },
       headers: {
         'Accept': 'application/json',
+      }
+    });
+
+    const response = await axios.get<IUserResponse>('https://api.github.com/user', {
+      headers: {
+        authorization: `Bearer ${accessTokenResponse.access_token}`,
       }
     });
 
