8664fd69294ccbada9b6d5cc9da0bb33741f3278
Jefferson Xavier <jeffersonx.xavier@gmail.com>
2021-11-03 11:34:57 -0300

    NLW7 - Getting messages from api

--- NLW7/web/src/components/MessageList/index.tsx
+++ NLW7/web/src/components/MessageList/index.tsx
@@ -1,39 +1,45 @@
+import { useEffect, useState } from 'react';
+import { api } from '../../services/api';
 import styles from './styles.module.scss';
 import LogoImg from '../../assets/logo.svg';
 
+type Message = {
+  id: string;
+  text: string;
+  user: {
+    name: string;
+    avatar_url: string;
+  }
+}
+
 export function MessageList () {
+
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  useEffect(() => {
+    api.get<Message[]>('messages/last3').then(response => {
+      setMessages(response.data);
+    });
+  }, []);
+
   return (
     <div className={styles.messageListWrapper}>
       <img src={LogoImg} alt="DoWhile 2021" />
 
       <ul className={styles.messageList}>
-        <li className={styles.message}>
-          <p className={styles.messageContent}>Não veja a hora de começar esse evento. Com certeza vai ser o melhor de todos os tempos, vamos pra cima!</p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img src="https://github.com/jeffersonxavier.png" alt="Jefferson Xavier" />
+        {
+          messages.map(message => (
+          <li key={message.id} className={styles.message}>
+            <p className={styles.messageContent}>{message.text}</p>
+            <div className={styles.messageUser}>
+              <div className={styles.userImage}>
+                <img src={message.user.avatar_url} alt={message.user.name} />
+              </div>
+              <span>{message.user.name}</span>
             </div>
-            <span>Jefferson Xavier</span>
-          </div>
-        </li>
-        <li className={styles.message}>
-          <p className={styles.messageContent}>Não veja a hora de começar esse evento. Com certeza vai ser o melhor de todos os tempos, vamos pra cima!</p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img src="https://github.com/jeffersonxavier.png" alt="Jefferson Xavier" />
-            </div>
-            <span>Jefferson Xavier</span>
-          </div>
-        </li>
-        <li className={styles.message}>
-          <p className={styles.messageContent}>Não veja a hora de começar esse evento. Com certeza vai ser o melhor de todos os tempos, vamos pra cima!</p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img src="https://github.com/jeffersonxavier.png" alt="Jefferson Xavier" />
-            </div>
-            <span>Jefferson Xavier</span>
-          </div>
-        </li>
+          </li>
+          ))
+        }
       </ul>
     </div>
   )
