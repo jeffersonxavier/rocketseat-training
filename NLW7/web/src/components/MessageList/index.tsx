49aa64abbb0f856d710410da44e919421cf716fc
Jefferson Xavier <jeffersonx.xavier@gmail.com>
2021-11-03 10:28:11 -0300

    NLW7 - Structuring messages list component

--- NLW7/web/src/components/MessageList/index.tsx
+++ NLW7/web/src/components/MessageList/index.tsx
@@ -1,5 +1,40 @@
+import styles from './styles.module.scss';
+import LogoImg from '../../assets/logo.svg';
+
 export function MessageList () {
   return (
-    <h1>MessageList</h1>
+    <div className={styles.messageListWrapper}>
+      <img src={LogoImg} alt="DoWhile 2021" />
+
+      <ul className={styles.messageList}>
+        <li className={styles.message}>
+          <p className={styles.messageContent}>Não veja a hora de começar esse evento. Com certeza vai ser o melhor de todos os tempos, vamos pra cima!</p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img src="https://github.com/jeffersonxavier.png" alt="Jefferson Xavier" />
+            </div>
+            <span>Jefferson Xavier</span>
+          </div>
+        </li>
+        <li className={styles.message}>
+          <p className={styles.messageContent}>Não veja a hora de começar esse evento. Com certeza vai ser o melhor de todos os tempos, vamos pra cima!</p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img src="https://github.com/jeffersonxavier.png" alt="Jefferson Xavier" />
+            </div>
+            <span>Jefferson Xavier</span>
+          </div>
+        </li>
+        <li className={styles.message}>
+          <p className={styles.messageContent}>Não veja a hora de começar esse evento. Com certeza vai ser o melhor de todos os tempos, vamos pra cima!</p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img src="https://github.com/jeffersonxavier.png" alt="Jefferson Xavier" />
+            </div>
+            <span>Jefferson Xavier</span>
+          </div>
+        </li>
+      </ul>
+    </div>
   )
 }
